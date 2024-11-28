f5983f4adf66c2fd2370f6c0710f334520b92d74
Amey Gaikar <gaikaramey04@gmail.com>
2024-11-28 12:58:04 +0530

    Passing functions via props

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,11 +1,20 @@
 import Alert from "./components/Alert";
+import ListGroup from "./components/ListGroup";
+
+const items = ["New York", "Los Angeles", "Chicago", "Texas", "Ohio"];
+
+const hanldeSelectItem = (item: string) => {
+  console.log(item);
+};
 
 function App() {
   return (
     <div>
-      <Alert>
-        Hello <strong> World!  </strong>
-      </Alert>
+      <ListGroup
+        items={items}
+        heading="cities"
+        onSelectItem={hanldeSelectItem}
+      />
     </div>
   );
 }
