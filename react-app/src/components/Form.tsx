ee399b87ce1a2989bd7698f1393089f68754670d
Amey Gaikar <gaikaramey04@gmail.com>
2025-01-31 14:53:19 +0530

    Applying validation on input fields, adding constraints for filling out input fields.

--- react-app/src/components/Form.tsx
+++ react-app/src/components/Form.tsx
@@ -1,9 +1,13 @@
 import { FormEvent, useRef, useState } from "react";
 import {FieldValues, useForm} from "react-hook-form";
 
+interface FormData {
+  name: string,
+  age: number
+}
+
 const Form = () => {
-  const {register, handleSubmit} = useForm();
-  console.log(register('name'));
+  const {register, handleSubmit, formState: {errors}} = useForm<FormData>();
 
   const onSubmitForm = (data: FieldValues) => {
     console.log(data)
@@ -15,14 +19,16 @@ const Form = () => {
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input {...register('name')} id="name" type="text" className="form-control"/>
+        <input {...register('name', {required: true, minLength: 3})} id="name" type="text" className="form-control"/>
+        {errors.name?.type === 'required' && <p className="text-danger">The name field is required.</p> || errors.name?.type === 'minLength' && <p className="text-danger">The name field should be more then 3 letters.</p> }
       </div>
 
       <div className="mb-3">
         <label htmlFor="" className="form-label">
           Age
         </label>
-        <input {...register('age')} type="number" className="form-control" />
+        <input {...register('age', {required: true})} type="number" className="form-control" />
+        {errors.age?.type === 'required' && <p className="text-danger">Age field is required.</p>}
       </div>
 
       <button className="btn btn-primary" type="submit">
