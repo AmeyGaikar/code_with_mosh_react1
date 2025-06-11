e2d96096cb44a39669931832b3bd2ac94d28dcd5
Amey Gaikar <gaikaramey04@gmail.com>
2025-06-11 15:18:34 +0530

    Schema based validation using zod

--- react-app/src/components/Form.tsx
+++ react-app/src/components/Form.tsx
@@ -1,16 +1,20 @@
 import { FormEvent, useRef, useState } from "react";
 import {FieldValues, useForm} from "react-hook-form";
+import { z } from "zod"
+import {zodResolver } from "@hookform/resolvers/zod"
 
-interface FormData {
-  name: string,
-  age: number
-}
+ const schema = z.object({
+   name: z.string().nonempty({ message: 'Name field is required' }).min(3, { message: 'Name must be at least 3 characters' }),
+  age: z.number({invalid_type_error: "Age field is required"}).min(18, {message: 'Age must be atleast 18'})  
+})
+
+type FormData =  z.infer<typeof schema>; 
 
 const Form = () => {
-  const {register, handleSubmit, formState: {errors}} = useForm<FormData>();
+  const {register, handleSubmit, formState: {errors}} = useForm<FormData>({resolver: zodResolver(schema)});
 
   const onSubmitForm = (data: FieldValues) => {
-    console.log(data)
+    console.log(data);
   }
 
   return (
@@ -19,16 +23,16 @@ const Form = () => {
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input {...register('name', {required: true, minLength: 3})} id="name" type="text" className="form-control"/>
-        {errors.name?.type === 'required' && <p className="text-danger">The name field is required.</p> || errors.name?.type === 'minLength' && <p className="text-danger">The name field should be more then 3 letters.</p> }
+        <input {...register('name')} id="name" type="text" className="form-control"/>
+        {errors.name && <p className="text-danger">{errors.name.message}</p>  }
       </div>
 
       <div className="mb-3">
         <label htmlFor="" className="form-label">
           Age
         </label>
-        <input {...register('age', {required: true})} type="number" className="form-control" />
-        {errors.age?.type === 'required' && <p className="text-danger">Age field is required.</p>}
+        <input {...register('age', {valueAsNumber: true})} type="number" className="form-control" />
+        {errors.age && <p className="text-danger">{errors.age.message}</p>  }
       </div>
 
       <button className="btn btn-primary" type="submit">
